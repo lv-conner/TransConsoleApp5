00c2510ab9a540a975db7358e01d3dc85de16aab
18010105 <18010105@PRCNMG1L0311>
2018-01-24 11:39:14 +0800

    表达式树转换和定义步骤

--- TransConsoleApp5/Program.cs
+++ TransConsoleApp5/Program.cs
@@ -25,7 +25,7 @@ namespace TransConsoleApp5
                 studentId = "01020"
             };
 
-            //Person person = TransObj<Student, Person>.Trans(student);
+            Person person = TransObj<Student, Person>.Trans(student);
             //Console.WriteLine(person.id);
             //Console.WriteLine(person.name);
             //Student student1 = TransObj<Person,Student>.TransToVersion2(p);
@@ -34,6 +34,27 @@ namespace TransConsoleApp5
             ConstantExpression constant = Expression.Constant(10, typeof(int));
             ConstantExpression constant1 = Expression.Constant(200,typeof(int));
             BinaryExpression binaryExpression = Expression.Add(constant1, constant);
+
+            ParameterExpression paraLeft = Expression.Parameter(typeof(int), "a");
+            ParameterExpression paraRight = Expression.Parameter(typeof(int), "b");
+
+            BinaryExpression binaryLeft = Expression.Multiply(paraLeft, paraRight);
+            ConstantExpression conRight = Expression.Constant(2, typeof(int));
+
+            BinaryExpression binaryBody = Expression.Add(binaryLeft, conRight);
+
+            LambdaExpression lambda =
+                Expression.Lambda<Func<int, int, int>>(binaryBody, paraLeft, paraRight);
+
+            Console.WriteLine(lambda.ToString());
+
+            Console.Read();
+
+
+
+
+
+
             Func<int> func = Expression.Lambda<Func<int>>(binaryExpression).Compile();
             Console.WriteLine(func());
             Console.ReadKey();
---
+++ TransConsoleApp5/ReadMe.txt
@@ -0,0 +1,24 @@
+﻿.Lambda #Lambda1<System.Func`2[TransConsoleApp5.Student,TransConsoleApp5.Person]>(TransConsoleApp5.Student $p) {
+    .New TransConsoleApp5.Person(){
+        id = $p.id,
+        name = $p.name
+    }
+}
+
+
+.New TransConsoleApp5.Person(){
+    id = $p.id,
+    name = $p.name
+}
+
+
+
+
+通过表达式构造方法：
+1、定义ParameterExpression（定义参数）
+2、定义操作（加减乘除，赋值，新建对象等相关操作）
+3、转换为Lambda表达式树（第一个为需要转换的表达式，第二个为参数）
+
+
+
+
