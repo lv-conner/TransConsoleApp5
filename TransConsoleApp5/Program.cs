891c8fb080d4d92e85fd4102bb299f69388a4099
18010105 <18010105@PRCNMG1L0311>
2018-01-24 11:45:16 +0800

    lambdaExpression tree excample

--- TransConsoleApp5/Program.cs
+++ TransConsoleApp5/Program.cs
@@ -31,32 +31,45 @@ namespace TransConsoleApp5
             //Student student1 = TransObj<Person,Student>.TransToVersion2(p);
             //定义一个变量。
             //var e = Expression.New(typeof(Person));
-            ConstantExpression constant = Expression.Constant(10, typeof(int));
-            ConstantExpression constant1 = Expression.Constant(200,typeof(int));
-            BinaryExpression binaryExpression = Expression.Add(constant1, constant);
+            //ConstantExpression constant = Expression.Constant(10, typeof(int));
+            //ConstantExpression constant1 = Expression.Constant(200,typeof(int));
+            //BinaryExpression binaryExpression = Expression.Add(constant1, constant);
 
-            ParameterExpression paraLeft = Expression.Parameter(typeof(int), "a");
-            ParameterExpression paraRight = Expression.Parameter(typeof(int), "b");
+            //ParameterExpression paraLeft = Expression.Parameter(typeof(int), "a");
+            //ParameterExpression paraRight = Expression.Parameter(typeof(int), "b");
 
-            BinaryExpression binaryLeft = Expression.Multiply(paraLeft, paraRight);
-            ConstantExpression conRight = Expression.Constant(2, typeof(int));
+            //BinaryExpression binaryLeft = Expression.Multiply(paraLeft, paraRight);
+            //ConstantExpression conRight = Expression.Constant(2, typeof(int));
 
-            BinaryExpression binaryBody = Expression.Add(binaryLeft, conRight);
+            //BinaryExpression binaryBody = Expression.Add(binaryLeft, conRight);
 
-            LambdaExpression lambda =
-                Expression.Lambda<Func<int, int, int>>(binaryBody, paraLeft, paraRight);
+            //LambdaExpression lambda =
+            //    Expression.Lambda<Func<int, int, int>>(binaryBody, paraLeft, paraRight);
 
-            Console.WriteLine(lambda.ToString());
+            //Console.WriteLine(lambda.ToString());
 
-            Console.Read();
-
-
+            //Console.Read();
 
 
 
 
-            Func<int> func = Expression.Lambda<Func<int>>(binaryExpression).Compile();
-            Console.WriteLine(func());
+
+
+
+
+            ParameterExpression parameterExpression = Expression.Parameter(typeof(int), "i");
+
+            BinaryExpression binaryExpression = Expression.AddAssign(parameterExpression, parameterExpression);
+
+            var ex = Expression.Lambda<Func<int, int>>(binaryExpression, parameterExpression);
+
+            Func<int, int> lambdaExpression = Expression.Lambda<Func<int, int>>(binaryExpression, parameterExpression).Compile();
+
+
+
+
+            Console.WriteLine(lambdaExpression(10));
+
             Console.ReadKey();
         }
     }
